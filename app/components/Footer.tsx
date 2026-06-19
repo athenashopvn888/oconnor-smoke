a73a66e03e0a5a7e7f8867d774660a6597c80dca
athenashopvn888 <athenashopvn888@gmail.com>
2026-06-19 12:04:44 +0700

    Add GBP local SEO landing pages with manual overrides

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,6 +74,7 @@ export default function Footer() {
               <Link href="/info/cheap-weed-york">Cheap Weed GTA</Link>
               <Link href="/info/native-cigarettes-york">Native Cigarettes</Link>
               <Link href="/info/weed-store-near-mississauga">Weed Store Near Mississauga</Link>
+              <Link href="/weed-dispensary-toronto/">OCONNOR SMOKE Weed Dispensary in Toronto</Link>
               <Link href="/contact">Contact Us</Link>
               <a
                 href="https://oconnorsmokecannabis.com/"
